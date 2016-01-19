619d577986c4089fa58916588618b765bb4b8f97
Mjollnir94 <marcel_hollink@hotmail.com>
2016-01-19 17:10:07 +0100

    GDX init
    Created projected files with the Gdx-setup.jar

---
+++ desktop/src/nl/icode4living/projectm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.icode4living.projectm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.icode4living.projectm.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
